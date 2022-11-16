a64f8a8f7235b7486f67ce59639bdc3d359570ba
georg <georg@DESKTOP-G9H7G2G>
2022-11-17 01:52:40 +0200

    Add project files.

---
+++ Pop_Georgiana_Lab2/Pages/Books/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Pop_Georgiana_Lab2.Data;
+using Pop_Georgiana_Lab2.Models;
+
+namespace Pop_Georgiana_Lab2.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Pop_Georgiana_Lab2.Data.Pop_Georgiana_Lab2Context _context;
+
+        public IndexModel(Pop_Georgiana_Lab2.Data.Pop_Georgiana_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Book != null)
+            {
+                Book = await _context.Book
+                  .Include(b => b.Publisher)
+                  .ToListAsync();
+            }
+        }
+    }
+}
---
+++ Pop_Georgiana_Lab2/Pages/Publishers/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Pop_Georgiana_Lab2.Data;
+using Pop_Georgiana_Lab2.Models;
+
+namespace Pop_Georgiana_Lab2.Pages.Publishers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Pop_Georgiana_Lab2.Data.Pop_Georgiana_Lab2Context _context;
+
+        public IndexModel(Pop_Georgiana_Lab2.Data.Pop_Georgiana_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Publisher> Publisher { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Publisher != null)
+            {
+                Publisher = await _context.Publisher.ToListAsync();
+            }
+        }
+    }
+}
